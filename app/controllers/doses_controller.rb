d238fadd5322110d3a3d0b9569ff4415c4254487
Manon Oriol <oriol.manon@gmail.com>
2020-11-12 17:01:34 +0100

    all the methods operational

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,39 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # cocktail id dans l'url
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail), notice: 'Dose was successfully added to the cocktail.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
+
+
+def destroy
+  @review = Review.find(params[:id])
+  restaurant = @review.restaurant
+  @review.destroy
+  redirect_to restaurant_path(restaurant)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    # resources :ingredients, only: [:index, :show]
+    resources :doses, only: [:new, :create]
+    # new_cocktail_dose GET    /cocktails/:cocktail_id/doses/new(.:format)
+  end
+  resources :doses, only: :destroy
 end
